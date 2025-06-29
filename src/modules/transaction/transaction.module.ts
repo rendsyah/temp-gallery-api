b797590315bd6287dfa8fcb3e34a995b717c3029
rendsyah <rndyfrdynsyh@gmail.com>
2025-06-29 16:10:12 +0700

    feat(transaction): import ProductModule in TransactionModule

--- src/modules/transaction/transaction.module.ts
+++ src/modules/transaction/transaction.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Transactions } from 'src/datasources/entities';
 
+import { ProductModule } from '../product/product.module';
+
 import { TransactionController } from './transaction.controller';
 import { TransactionService } from './transaction.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transactions])],
+  imports: [TypeOrmModule.forFeature([Transactions]), ProductModule],
   controllers: [TransactionController],
   providers: [TransactionService],
 })
