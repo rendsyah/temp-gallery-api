69ed06d78bf3902f6269c16613654ee85f42ebf1
rendsyah <rndyfrdynsyh@gmail.com>
2025-06-29 16:06:47 +0700

    test(menu): add repository mock and test setup for MenuService

--- src/modules/master/menu/menu.service.spec.ts
+++ src/modules/master/menu/menu.service.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { MasterMenu } from 'src/datasources/entities';
+
 import { MenuService } from './menu.service';
 
 describe('MenuService', () => {
   let service: MenuService;
 
+  let menuRepository: jest.Mocked<Repository<MasterMenu>>;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MenuService],
+      providers: [
+        MenuService,
+        {
+          provide: getRepositoryToken(MasterMenu),
+          useValue: {
+            createQueryBuilder: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<MenuService>(MenuService);
+
+    menuRepository = module.get(getRepositoryToken(MasterMenu));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getDetailContact', () => {
+    console.log(menuRepository);
   });
 });
