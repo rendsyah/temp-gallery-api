868bb491a6850f9ea73f27d1eb752bf27b253b31
rendsyah <rndyfrdynsyh@gmail.com>
2025-07-22 18:00:28 +0700

    feat(master): add ArtistModule and ExhibitionsModule to imports

--- src/modules/master/master.module.ts
+++ src/modules/master/master.module.ts
@@ -1,13 +1,23 @@
 import { Global, Module } from '@nestjs/common';
 
 import { ArticleModule } from './article/article.module';
+import { ArtistModule } from './artist/artist.module';
 import { BannerModule } from './banner/banner.module';
 import { ContactModule } from './contact/contact.module';
+import { ExhibitionsModule } from './exhibitions/exhibitions.module';
 import { ParamsModule } from './params/params.module';
 import { MenuModule } from './menu/menu.module';
 
 @Global()
 @Module({
-  imports: [ArticleModule, BannerModule, ContactModule, MenuModule, ParamsModule],
+  imports: [
+    ArticleModule,
+    ArtistModule,
+    BannerModule,
+    ContactModule,
+    ExhibitionsModule,
+    MenuModule,
+    ParamsModule,
+  ],
 })
 export class MasterModule {}
