f102cb495af2b4b4724327d896e7c0e1ca85a61c
rendsyah <rndyfrdynsyh@gmail.com>
2025-07-27 12:57:15 +0700

    feat(commons): add FilesPipe for multiple files validation

--- src/commons/pipes/file.pipe.ts
+++ src/commons/pipes/file.pipe.ts
@@ -4,8 +4,18 @@ import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 export class FilePipe implements PipeTransform {
   transform(file: Express.Multer.File) {
     if (!file) {
-      throw new BadRequestException('File / image is required');
+      throw new BadRequestException('File is required');
     }
     return file;
+  }
+}
+
+@Injectable()
+export class FilesPipe implements PipeTransform {
+  transform(files: Express.Multer.File[]) {
+    if (!files || !Array.isArray(files) || files.length === 0) {
+      throw new BadRequestException('File(s) is required');
+    }
+    return files;
   }
 }
