589a6579bd437be300b79c2b6ba13d9318ff401c
rendsyah <rndyfrdynsyh@gmail.com>
2025-08-03 16:28:11 +0700

    feat(menu): enhance menu service with privilege joins and nested structure

--- src/modules/master/menu/menu.service.ts
+++ src/modules/master/menu/menu.service.ts
@@ -20,42 +20,72 @@ export class MenuService {
    */
   async getMenu(): Promise<MenuResponse> {
     const getMenu = await this.MenuRepository.createQueryBuilder('menu')
+      .innerJoin('menu.privilege', 'privilege')
       .select([
         'menu.id AS id',
         'menu.name AS name',
         'menu.path AS path',
         'menu.icon AS icon',
         'menu.level AS level',
-        'menu.header AS header',
+        'menu.parent_id AS parent_id',
+        'menu.is_group AS is_group',
+        'menu.sort AS sort',
+        'privilege.id AS privilege_id',
+        'privilege.menu_id AS menu_id',
+        'privilege.action AS action',
       ])
-      .orderBy('menu.level', 'ASC')
+      .orderBy('menu.parent_id', 'ASC')
       .addOrderBy('menu.sort', 'ASC')
-      .addOrderBy('menu.id', 'ASC')
+      .addOrderBy('privilege.id', 'ASC')
       .getRawMany();
 
-    const menuMap = new Map<number, IMenu>();
+    const result: IMenu[] = [];
+    const resultMap = new Map<number, IMenu>();
 
-    const result = getMenu.reduce<IMenu[]>((acc, menu) => {
-      const data: IMenu = {
-        id: menu.id,
-        name: menu.name,
-        path: menu.path,
-        icon: menu.icon,
-        level: menu.level,
-        child: [],
-      };
-
-      menuMap.set(data.id, data);
-
-      if (!menu.header) {
-        acc.push(data);
-      } else {
-        const parent = menuMap.get(menu.header);
-        if (parent) parent.child.push(data);
+    for (const menu of getMenu) {
+      if (!resultMap.has(menu.id)) {
+        resultMap.set(menu.id, {
+          id: menu.id,
+          name: menu.name,
+          path: menu.path,
+          icon: menu.icon,
+          level: menu.level,
+          is_group: menu.is_group,
+          actions: [],
+          child: [],
+        });
       }
 
-      return acc;
-    }, []);
+      const currentMenu = resultMap.get(menu.id)!;
+      currentMenu.actions?.push({
+        privilege_id: menu.privilege_id,
+        action: menu.action,
+      });
+
+      if (menu.parent_id === 0) {
+        if (!result.includes(currentMenu)) {
+          result.push(currentMenu);
+        }
+      } else {
+        if (!resultMap.has(menu.parent_id)) {
+          resultMap.set(menu.parent_id, {
+            id: menu.parent_id,
+            name: '',
+            path: '',
+            icon: '',
+            level: 0,
+            is_group: 1,
+            actions: [],
+            child: [],
+          });
+        }
+
+        const parentMenu = resultMap.get(menu.parent_id)!;
+        if (!parentMenu.child.includes(currentMenu)) {
+          parentMenu.child.push(currentMenu);
+        }
+      }
+    }
 
     return result;
   }
